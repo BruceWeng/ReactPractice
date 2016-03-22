4167d2ad3611363e441e93b590a0f89ce7e97703
BruceWeng <weng8916@gmail.com>
2016-03-22 12:16:10 -0700

    Adding Product Function

--- script.js
+++ script.js
@@ -37,7 +37,13 @@ var Total = React.createClass({
 var ProductForm = React.createClass({
   submit: function(e) {
     e.preventDefault();
-    alert('Name: ' + this.refs.name.value + ' - $' + this.refs.price.value);
+
+    var product ={
+      name: this.refs.name.value,
+      price: parseInt(this.refs.price.value)
+    }
+
+    this.props.handleCreate(product);
 
     this.refs.name.value = "";
     this.refs.price.value = "";
@@ -68,6 +74,13 @@ var ProductList = React.createClass({
       ]
     };
   },
+
+  createProduct: function(product) {
+    this.setState({
+      productList: this.state.productList.concat(product)
+    })
+  },
+
   calculateTotal: function(price) {
     this.setState({total: this.state.total + price});
   },
@@ -80,17 +93,17 @@ var ProductList = React.createClass({
     var component = this;
     var products = this.state.productList.map(function(product) {
       return (
-        <Product name = {product.name} price = {product.price}
-          handleShow = {component.showProduct}
-          handleTotal = {component.calculateTotal}/>
+        <Product name={product.name} price={product.price}
+          handleShow={component.showProduct}
+          handleTotal={component.calculateTotal}/>
       );
     });
 
     return (
       <div>
-        <ProductForm/>
+        <ProductForm handleCreate={this.createProduct}/>
         {products}
-        <Total total = {this.state.total}/>
+        <Total total={this.state.total}/>
       </div>
     )
   }
